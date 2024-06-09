7860304b8b8109ea1cd825f792571f53eac1ea8e
Pavel <pmshuv975@gmail.com>
2024-06-09 19:11:01 +0300

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,44 @@
+package ru.netology
+
+
+fun main() {
+    println(agoToText(68)) // минуту назад
+    println(agoToText(1_320)) // 22 минуты назад
+    println(agoToText(2_220)) // 37 минут назад
+    println(agoToText(75_600)) // 21 час назад
+    println(agoToText(79_200)) // 22  часа назад
+    println(agoToText(72_000)) // 20  часов назад
+    println(agoToText(170_000)) // вчера
+    println(agoToText(172_802)) // позавчера
+    println(agoToText(300_802)) // давно
+}
+
+fun agoToText(time: Int) = when (time) {
+    in 0..60 -> "только что"
+    in 61..3600 -> "был(а) " + "${minutes(time)} " + "${minutesStringEnd(time)} назад"
+    in 3601..86400 -> "был(а) " + "${hours(time)} " + "${hourstringEnd(time)} назад"
+    in 86401..172800 -> "был(а) вчера"
+    in 172801..259200 -> "был(а) позавчера"
+    else -> "был(а) давно"
+}
+fun minutes(time: Int) = time / 60
+
+fun minutesStringEnd(time: Int) = when (time / 60 % 10) {
+    1 -> "минуту"
+    2, 3, 4 -> "минуты"
+    else -> "минут"
+}
+
+fun hours(time: Int) = time / 3600
+
+fun hourstringEnd(time: Int) = when (time / 3600 % 10) {
+    1 -> "час"
+    2, 3, 4 -> "часа"
+    else -> "часов"
+}
+
+
+
+
+
+

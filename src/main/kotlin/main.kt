7c5f2a0419a77c538d58b03210a7a38d0dc8f660
Pavel <pmshuv975@gmail.com>
2024-06-15 00:17:33 +0300

    new time slot added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,8 +3,10 @@ package ru.netology
 
 fun main() {
     println(agoToText(68)) // минуту назад
+    println(agoToText(720)) // 12 минут назад
     println(agoToText(1_320)) // 22 минуты назад
     println(agoToText(2_220)) // 37 минут назад
+    println(agoToText(39_600)) // 11 часов назад
     println(agoToText(75_600)) // 21 час назад
     println(agoToText(79_200)) // 22  часа назад
     println(agoToText(72_000)) // 20  часов назад
@@ -23,21 +25,31 @@ fun agoToText(time: Int) = when (time) {
 }
 fun minutes(time: Int) = time / 60
 
-fun minutesStringEnd(time: Int) = when (time / 60 % 10) {
-    1 -> "минуту"
-    2, 3, 4 -> "минуты"
-    else -> "минут"
+fun minutesStringEnd(time: Int) : String {
+    return if (time / 60 in 11..14) "минут"
+    else when (time / 60 % 10) {
+        1 -> "минуту"
+        2, 3, 4 -> "минуты"
+        else -> "минут"
+    }
 }
 
 fun hours(time: Int) = time / 3600
 
-fun hourstringEnd(time: Int) = when (time / 3600 % 10) {
-    1 -> "час"
-    2, 3, 4 -> "часа"
-    else -> "часов"
+fun hourstringEnd(time: Int) : String {
+    return if (time / 3600 in 11..14) "часов"
+    else when (time / 3600 % 10) {
+        1 -> "час"
+        2, 3, 4 -> "часа"
+        else -> "часов"
+    }
 }
 
-
+//fun hourstringEnd(time: Int) = when (time / 3600 % 10) {
+//    1 -> "час"
+//    2, 3, 4 -> "часа"
+//    else -> "часов"
+//}
 
 
 
